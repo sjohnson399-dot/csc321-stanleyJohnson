26e5b80cecc0630514d49e831039c329c49fef2b
sjohnson399-dot <sjohnson399@toromail.csudh.edu>
2026-05-15 03:13:23 +0000

    what

--- stanPush.c
+++ stanPush.c
@@ -28,61 +28,59 @@ void stanPush(void)
 	do 
 	{
 
-        printf("\n=== stanley's room ===\n");
+        	printf("\n=== stanley's room ===\n");
 
-        for(int i = 0; i < 5; i++) 
-	{
-            printf("%d. Enter %s\n", i + 1, stanRooms[i]);
-        }
+        	for(int i = 0; i < 5; i++) 
+		{
+            		printf("%d. Enter %s\n", i + 1, stanRooms[i]);
+        	}
 
-        printf("6. quit\n");
+        	printf("6. quit\n");
 
-        printf("choose a room: ");
-        scanf("%d", &stanChoice);
+        	printf("choose a room: ");
+        	scanf("%d", &stanChoice);
 
-        if(stanChoice >= 1 && stanChoice <= 5) 
-	{
+        	if(stanChoice >= 1 && stanChoice <= 5) 
+		{
 
-            stanVisits[stanChoice - 1]++;
+            		stanVisits[stanChoice - 1]++;
 
-            printf("\nYou entered the %s.\n",
-                   stanRooms[stanChoice - 1]);
+            		printf("\nYou entered the %s.\n", stanRooms[stanChoice - 1]);
 
-            stanRandy = rand() % 3;
+            		stanRandy = rand() % 3;
 
-            if(stanRandy == 0) 
-	    {
-                printf("You found treasure!\n");
-            }
-            else if(stanRandy == 1) 
-	    {
-                printf("A monster appeared!\n");
-            }
-            else 
-	    {
-                printf("Nothing happened.\n");
-            }
+            		if(stanRandy == 0) 
+	    		{
+                		printf("You found treasure!\n");
+           		}
+            		else if(stanRandy == 1) 
+	    		{
+                		printf("A monster appeared!\n");
+            		}
+            		else 
+	    		{
+                		printf("Nothing happened.\n");
+            		}
 
-        }
-	else if(stanChoice == 6) 
-	{
+        	}
+		else if(stanChoice == 6) 
+		{
 
-            printf("\n=== rooms visits ===\n");
+            		printf("\n=== rooms visits ===\n");
 
-            for(int i = 0; i < 5; i++) {
+            		for(int i = 0; i < 5; i++) {
 
-                printf("%s visited %d time(s)\n",
-                       stanRooms[i],
-                       stanVisits[i]);
-            }
+                		printf("%s visited %d time(s)\n", stanRooms[i], stanVisits[i]);
+            	}
 
-            printf("Goodbye!\n");
-        }
-        else 
-	{
+            	printf("Goodbye!\n");
 
-            printf("Invalid choice.\n");
-        }
+        	}
+        	else 
+		{
+
+            		printf("Invalid choice.\n");
+        	}
 
     } while(stanChoice != 6);
 
