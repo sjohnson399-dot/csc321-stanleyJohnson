cfd362426d4bf27a656f0990b1ef4cc91c5c8b40
sjohnson399-dot <sjohnson399@toromail.csudh.edu>
2026-05-15 01:44:50 +0000

    a

--- sjohnson12.py
+++ sjohnson12.py
@@ -7,14 +7,11 @@ def calcRecArea(base, height):
 def calcCircleArea(radius):
     return math.pi * (radius ** 2) 
 
-def main(): 
-    base = float(input("Enter a base value: "))
-    height = float(input("Enter a height value: "))
-    areaRec = calcRecArea(base, height)
-    print("The area of a rectangle is ", areaRec)
+base = float(input("Enter a base value: "))
+height = float(input("Enter a height value: "))
+areaRec = calcRecArea(base, height)
+print("The area of a rectangle is ", areaRec)
 
-    radius = float(input("Enter a radius value: "))
-    areaCircle = calcCircleArea(radius)
-    print("The area of the circle is ", areaCircle)
-
-main()
+radius = float(input("Enter a radius value: "))
+areaCircle = calcCircleArea(radius)
+print("The area of the circle is ", areaCircle)
